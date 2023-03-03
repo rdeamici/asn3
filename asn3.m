2614b0dbf0ae754aefee27b4c191ae0fea85961f
rdeamici <rtdeamicis@gmail.com>
2023-03-03 00:33:23 -0800

    part2: have a formula for deriving C. not working properly. using svd() which may be incorrect

--- asn3.m
+++ asn3.m
@@ -1,82 +1,140 @@
-set1files = {'data\DSCF4177.jpg', 'data\DSCF4178.jpg', 'data\DSCF4179.jpg',...
-            'data\DSCF4180.jpg','data\DSCF4186.jpg', 'data\DSCF4187.jpg'};
-set1fileMasks = {'data\DSCF4177Mask.jpg', 'data\DSCF4178Mask.jpg', 'data\DSCF4179Mask.jpg', ...
-            'data\DSCF4180Mask.jpg','data\DSCF4186Mask.jpg', 'data\DSCF4187Mask.jpg'};
 
-images = {};
-masks = {};
+P =        [0,48,48; 32,48,48; 32,80,48; 0,48,67; 32,48,67; 32,80,67];
+Pc1_4177 = [424,912;  661,994;  841,823; 383,771;  634,848;  815,680];
 
-% Read in the images using imread and create binary masks using imbinarize
-for i = 1:numel(set1files)
-    images{i} = single(rgb2gray(imread(set1files{i})));
-    masks{i} = imbinarize(imread(set1fileMasks{i}), 0.5);
+U=[];
+S=[];
+V=[];
+C=[];
+[U, S, V, C] = find_c(P,Pc1_4177);
+U
+S
+V
+C
+
+% A_inv = V*inv(S)*U';
+
+img = im2gray(imread('data\DSCF4177.jpg'));
+size(img)
+% C*vertcat(P(1,:)', 1)
+
+function [lsv,sv,rsv,calibration] = find_c(P,Pc1)
+    % Build the linear system
+    A = [];
+    for i = 1:size(P,1)
+        X = P(i,1);
+        Y = P(i,2);
+        Z = P(i,3);
+        
+        u = Pc1(i,1);
+        v = Pc1(i,2);
+        A = [A; X, Y, Z, 1, 0, 0, 0, 0, -u*X, -u*Y, -u*Z;
+                 0, 0, 0, 0, X, Y, Z, 1, -v*X, -v*Y, -v*Z];
+    end
+    
+    % Solve for the camera calibration matrix
+    [lsv,sv,rsv] = svd(A);
+    c = rsv(:,end);
+    calibration = [c(1) c(2)  c(3)   c(4);
+                   c(5) c(6)  c(7)   c(8);
+                   c(9) c(10) c(11)  1];
 end
 
-% PART 1status
-for i=1:numel(images)-1
-    image1 = images{i};
-    mask1 = masks{i};
-    image2 = images{i+1};
-    mask2 = masks{i+1};
-
-    %compute SIFT features
-    [frames1, descriptors1] = vl_sift(image1);
-    [frames2, descriptors2] = vl_sift(image2);
-
-    % compute matches
-    [matches, scores] = vl_ubcmatch(descriptors1, descriptors2);
+% PART 1
+function s = part1_setup
+    set1files = {'data\DSCF4177.jpg', 'data\DSCF4178.jpg', 'data\DSCF4179.jpg',...
+                'data\DSCF4180.jpg','data\DSCF4186.jpg', 'data\DSCF4187.jpg'};
+    set1fileMasks = {'data\DSCF4177Mask.jpg', 'data\DSCF4178Mask.jpg', 'data\DSCF4179Mask.jpg', ...
+               'data\DSCF4180Mask.jpg','data\DSCF4186Mask.jpg', 'data\DSCF4187Mask.jpg'};
     
-    % sort matches based on the associated scores
-    [scores_sorted, idx_in_orig] = sort(scores);
-    matches_sorted = matches(:, idx_in_orig);
+    masks = cell(length(set1files), 1);
+    images = cell(length(set1files), 1);
+    sift_features = cell(length(set1files), 2);
     
-    % get all frames from f1 and f2 that are associated with a match
-    matches1 = matches(1,:);
-    matches2 = matches(2,:);
-    f1_matches = frames1(:,matches1);
-    f2_matches = frames2(:,matches2);
-
-    % TODO: find all possible pixels that lie on a frame
-
-    %       need to find all pixels that lie on each frame
-    %       centered at (x,y), with radius r
-    %       pixel_indices_within_range returns the top left and bottom
-    %       right position of the square that contains the given circle for
-    %       a frame. We need to get this box that contians the frame, then
-    %       check in each(? not sure about this part) mask for both images
-    %       to see if any of the pixels in the box contain a part of the
-    %       feature described by the frame.
-
-    %       need to look further at the documentation for the vlfeat
-    %       package...
+    % Read in the images using imread and create binary masks using imbinarize
+    for i = 1:numel(set1files)
+        img = imread(set1files{i});
+        images{i} = rgb2gray(img);
+        single_gray_image = single(rgb2gray(img));
+        
+        % compute SIFT features
+        [frames, descriptors] = vl_sift(single_gray_image, 'PeakThresh',0);
+        sift_features{i, 1} = frames;
+        sift_features{i, 2} = descriptors;
+        
+        % create binary mask from mask file
+        mask = rgb2gray(imread(set1fileMasks{i}));
+        mask = mask>0;
+        masks{i} = mask;
+    end
+    
+    s = size(images{1});    
 end
 
-function [top, left, bottom, right] = pixel_indices_within_range_of_frame(frame, s)
+function [frames_on_object1, frames_on_object2] = part1(images, sift_features, masks)
+    for i=1:numel(images)-1
+        image1 = images{i};
+        mask1 = masks{i};
+        image2 = images{i+1};
+        mask2 = masks{i+1};
+    
+        frames1 = sift_features{i,1};
+        frames2 = sift_features{i+1,1};
+        descriptors1 = sift_features{i,2};
+        descriptors2 = sift_features{i+1,2};
+    
+    
+        % compute matches
+        [matches, scores] = vl_ubcmatch(descriptors1, descriptors2);
+        % sort matches based on the associated scores
+        [scores_sorted, idx_in_orig] = sort(scores);
+        matches_sorted = matches(:, idx_in_orig);
+        
+        % remove matches with very low scores
+    %     below_thresh = scores_sorted < 1000;
+    %     matches_sorted = matches_sorted(:, below_thresh);
+        
+        % get all frames from f1 and f2 that are associated with a match
+        matches1 = matches_sorted(1,:);
+        matches2 = matches_sorted(2,:);
+        f1_matches = frames1(:, matches1);
+        f2_matches = frames2(:, matches2);
+    %     whos f2_matches
+        figure, imshow(image1);
+        vl_plotframe(f1_matches);
+        figure, imshow(image2);
+        vl_plotframe(f2_matches);
+    
+        % remove SIFT features which are not on the object
+        f1_mask = mask1(sub2ind(size(mask1), round(f1_matches(2,:)), round(f1_matches(1,:))));
+        frames_on_object1 = f1_matches(:,f1_mask);
+        f2_mask = mask2(sub2ind(size(mask2), round(f2_matches(2,:)), round(f2_matches(1,:))));
+        frames_on_object2 = f2_matches(:,f2_mask);
+        figure, imshow(image1);
+        vl_plotframe(frames_on_object1);
+        figure, imshow(image2);
+        vl_plotframe(frames_on_object2);
+    end
+end
+
+function [top, left, bottom, right] = pixel_indices_within_range_of_feature(frame, s)
     row = frame(1);
     col = frame(2);
     rad = frame(3);
+
     % Determine edges of the box
-    left = col - r;
-    right = col + r;
-    top = row - r;
-    bottom = row + r;
+    left = floor(col - rad);
+    right = ceil(col + rad);
+    top = floor(row - rad);
+    bottom = ceil(row + rad);
     
     % Adjust edges to image boundaries
-    [rows, cols] = s;
+    rows = s(1);
+    cols = s(2);
     left = max(1, left);
     right = min(cols, right);
     top = max(1, top);
     bottom = min(rows, bottom);
-    
-    % Calculate width and height of the box
-    width = right - left + 1;
-    height = bottom - top + 1;
-    
-    % Return coordinates of top-left and bottom-right corners
-    x1 = left;
-    y1 = top;
-    x2 = left + width - 1;
-    y2 = top + height - 1;
 end
 
 function [valid_frames, valid_descriptors] = find_valids(frames, descriptors, mask1, mask2)
